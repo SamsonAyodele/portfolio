db7764d599fb3f0234f6e518f537844678bd5480
Samson-Ayodele <idowusamson0777@gmail.com>
2024-05-02 07:52:37 +0100

    Modified

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -26,7 +26,7 @@ const Nav = (props) => {
           </ul>
         </div>
 
-        <div className="toggler">
+        {/* <div className="toggler">
           <p className="toggler--light">Light</p>
 
           <div className="toggler--slider" onClick={props.toggleDarkMode}>
@@ -34,16 +34,10 @@ const Nav = (props) => {
           </div>
 
           <p className="toggler--dark">Dark</p>
-        </div>
+        </div> */}
 
         <div class="hamburger">
-          <svg
-            width="18"
-            height="15"
-            viewBox="0 0 18 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M1 13.5H17H1ZM1 1.5H17H1ZM1 7.5H17H1Z"
               stroke="black"
